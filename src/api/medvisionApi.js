5122b46b6543f6bb492836b31573fc228967fdd6
Alexmwale <alexmwalec03@gmail.com>
2026-08-27 15:44:12 +0200

    Changing  API_BASE_URL to match with docker URL on backend

--- src/api/medvisionApi.js
+++ src/api/medvisionApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = (process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api").replace(/\/$/, "");
+const API_BASE_URL = (process.env.REACT_APP_API_BASE_URL || "http://localhost:8000").replace(/\/$/, "");
 const API_ORIGIN = API_BASE_URL.replace(/\/api$/, "");
 
 const parseJson = async (response) => {
